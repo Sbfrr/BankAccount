3716dccd8524524ebd13e3e4489dfb7b81a99634
Sebastien Ferre <sebastien.ferre@sundayapp.com>
2022-05-19 10:47:44 +0200

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/main/java/org/bank/account/domain/BankAccount.java
@@ -0,0 +1,7 @@
+package org.bank.account.domain;
+
+public class BankAccount {
+    public Integer balance() {
+        return null;
+    }
+}
--- src/test/java/org/bank/account/domain/BankAccountTest.java
+++ src/test/java/org/bank/account/domain/BankAccountTest.java
@@ -1,10 +1,12 @@
 package org.bank.account.domain;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
 public class BankAccountTest {
     @Test
     public void initial_balance_is_zero() {
-
+        var newBankAccount = new BankAccount();
+        assertThat(newBankAccount.balance()).isEqualTo(0);
     }
 }
